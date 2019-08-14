11d01d6139b8925bac78de0d4165a3546f24bee9
gesila <370225203@qq.com>
2019-08-14 15:44:46 +0800

    add test inheritance code to run in browser

---
+++ dome.js
@@ -0,0 +1,10 @@
+(function() {
+  // 测试 6种继承方式运行情况
+  console.log('测试 6种继承方式运行情况');
+  children1.say();
+  children2.say();
+  children3.say();
+  children4.say();
+  children5.say();
+  children6.say();
+})();
--- js_inheritance_note.js
+++ js_inheritance_note.js
@@ -4,7 +4,7 @@
 function father(name) {
   this.name = name;
   this.say = function() {
-    console.log('say');
+    console.log(`say${name}`);
   };
 }
 father.prototype.action = function() {
@@ -12,16 +12,18 @@ father.prototype.action = function() {
 };
 
 //  原型链继承
-function children1() {
-  this.name = 'children1';
+function childrenOne(name) {
+  this.name = name;
 }
-children1.prototype = new father();
+childrenOne.prototype = new father();
+let children1 = childrenOne('children1');
 
 //  构造函数继承
-function children2(name) {
+function childrenTwo(name) {
   father.call(this, name);
   this.age = 8;
 }
+let children2 = new childrenTwo('children2');
 
 /* class classType {
   constructor() {
@@ -31,36 +33,45 @@ function children2(name) {
 } */
 
 //  组合继承，组合原型链继承和构造函数继承
-function children3(name) {
+function childrenThree(name) {
   father.call(this, name);
 }
-children3.prototype = new father();
+childrenThree.prototype = new father();
+let children3 = new childrenThree('children3');
 
 //  原型链继承
-(function() {
-  //  输出对象，承载继承的原型
-  function contain(obj) {
-    function fn() {}
-    fn.prototype = obj;
-    return new fn();
-  }
-  let fa = new father();
-  let children4 = contain(fa);
-});
+//  输出对象，承载继承的原型
+function contain(obj) {
+  function fn() {}
+  fn.prototype = obj;
+  return new fn();
+}
+let fa4 = new father();
+let children4 = contain(fa4);
 
 //  寄生式继承
-(function() {
-  //  输出对象，承载继承的原型
-  function contain(obj) {
-    function fn() {}
-    fn.prototype = obj;
-    return new fn();
-  }
-  //  原型链继承再套一层，传递参数
-  function subObject(obj, name) {
-    let sub = contain(obj);
-    sub.name = name;
-  }
-  let fa = new father();
-  let children4 = subObject(fa, 'children4');
-});
+//  输出对象，承载继承的原型
+/* function contain(obj) {
+  function fn() {}
+  fn.prototype = obj;
+  return new fn();
+} */
+
+//  原型链继承再套一层，传递参数
+function subObject(obj, name) {
+  let sub = contain(obj);
+  sub.name = name;
+}
+let fa5 = new father();
+let children5 = subObject(fa5, 'children5');
+
+//  寄生组合式继承
+//  组合 解决组合式两次调用构造函数属性的缺点
+function sub(name) {
+  father.call(this, name);
+}
+
+let fa6 = contain(father.prototype);
+sub.prototype = fa6;
+fa6.constructor = sub;
+let children6 = new sub('children6');
